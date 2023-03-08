a920bf4d0efd97eb305ca96bb8cc3291fc1781a9
Clare Tubridy <G00354146@atu.ie>
2023-03-08 15:28:37 +0000

    Week 05 Lab

--- week05/weekday.py
+++ week05/weekday.py
@@ -1,8 +1,8 @@
 # Program determines whether today is a weekday or weekend.
 # Author: Clare Tubridy
-# Date: 01/03/2023
+# Date: 08/03/2023
 
-import datetime
+import datetime                                     # Importing the datetime module allows programe to work with dates as date objects
 
 current_date = datetime.datetime.now()              # Determines current date
 day = current_date.weekday()                        # Determines the day of the week
