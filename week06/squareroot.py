68fa003773904e924ac9e0ba4cb974c6922fa4f7
Clare Tubridy <G00354146@atu.ie>
2023-03-27 15:51:05 +0100

    Week 6 Task Completed

--- week06/squareroot.py
+++ week06/squareroot.py
@@ -3,30 +3,31 @@
 # Author: Clare Tubridy
 # 09-03-2023
 
-# Function to ensure the input is a positive integer
+# To ensure the input is a positive integer
+number = float(input("Please enter a postive number: "))
+while number < 1:
+    print("This is not a positive number")
+    number = float(input("Try again! Any postive number: "))
 
-def readNum():
-    number = float(input("Please enter a postive number: "))
-    while number < 1:
-        print("This is not a positive number")
-        number = float(input("Try again! Any postive number: "))
 
-num = readNum()
+# Function to return the sqaureroot of a number using Newtons Method
+def sqrt(number):
 
-# Function to return the sqaureroot of a number
-# Using Newtons Method
-def sqrt(num):
-
-    x = num                        # set inital guess for sqaure root
+    x = number                              # set inital guess for sqaure root
+    count = 0                               # count numbers of iterations
 
     while True:
+        count += 1
 
-        root = 0.5 * (x + (num / x))
+        root = 0.5 * (x + (number / x))    
 
         if abs(root - x) < 1e-9:
-            return root
+            break
+        x = root                            # update root
 
-        #x = root
+    return root
 
-print(f"The sqaure root of {num} is approx {sqrt(num)}")
+n = round(sqrt(number),1)                   # round to one decimal place
+
+print(f"The sqaure root of {number} is approx {n}")
 
