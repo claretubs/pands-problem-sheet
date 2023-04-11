4971a25b707396e37318f731e28c7509015f87e6
Clare Tubridy <G00354146@atu.ie>
2023-04-11 14:00:54 +0100

    Accounts Task

--- week03/accounts.py
+++ week03/accounts.py
@@ -1,7 +1,7 @@
-#Program that reads 10 character account numbers
-#It only outputs the last 4 digits and the first 6 are replaced by x's
-#Author: Clare Tubridy
-#Date: 09/02/2023
+# Program that reads 10 character account numbers
+# It only outputs the last 4 digits and the first 6 are replaced by x's
+# Author: Clare Tubridy
+# Date: 09/02/2023
 
 def replaced_account_number(account_number):
     if len(account_number) <=4:
@@ -18,4 +18,4 @@ account_number = input("Please enter your 10 digit account number: ")
 
 new_account_number = replaced_account_number(account_number)
 
-print(f"Your account number is:{new_account_number}")
+print(f"Your account number is: {new_account_number}")
