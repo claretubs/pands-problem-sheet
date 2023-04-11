6f6901410c166b49c3f533788333e594e18102ef
Clare Tubridy <G00354146@atu.ie>
2023-04-11 14:34:48 +0100

    Bank Task

--- week02/bank.py
+++ week02/bank.py
@@ -1,11 +1,11 @@
-#bank.py
-#Author: Clare Tubridy
-#Date: 07/02/2023
-#Reads two amounts in cents, adds them together and outputs the euro decimal amount.
+# bank.py
+# Author: Clare Tubridy
+# Date: 07/02/2023
+# Reads two amounts in cents, adds them together and outputs the euro decimal amount.
 
-money1=int(input("Please enter your first amount in cents: "))     #inputs amount 1 in cents
-money2=int(input("Please enter your second amount in cents: "))    #inputs amount 2 in cents
+money1=int(input("Please enter your first amount in cents: "))     # inputs amount 1 in cents
+money2=int(input("Please enter your second amount in cents: "))    # inputs amount 2 in cents
 
-tmoney=(money1+money2)/100  #finds total amount of money in euro
+tmoney=(money1+money2)/100  # finds total amount of money in euro
 
 print(f"The total amount of money is €{tmoney}")
