ea45586fcf4057efce351e695a2705f715caf305
Clare Tubridy <G00354146@atu.ie>
2023-04-18 13:01:00 +0100

    Bank Task Rework

--- bank.py
+++ bank.py
@@ -3,9 +3,19 @@
 # Date: 07/02/2023
 # Reads two amounts in cents, adds them together and outputs the euro decimal amount.
 
-money1=int(input("Please enter your first amount in cents: "))     # inputs amount 1 in cents
-money2=int(input("Please enter your second amount in cents: "))    # inputs amount 2 in cents
+# Prompts user to enter amount 1 in cents
+money1=int(input("Please enter your first amount in cents: "))
+# Prompts user to enter amount 2 in cents
+money2=int(input("Please enter your second amount in cents: "))
 
-tmoney=(money1+money2)/100  # finds total amount of money in euro
+# Adds the two amounts together.
+total_amount = money1 + money2
 
-print(f"The total amount of money is €{tmoney}")
+# Integer division('//') instead of regular division('/')
+# Finds total amount in euro
+euros = total_amount // 100
+# Modulus used to find remainder. In this case the remainder is equal to cents
+cents = total_amount % 100
+
+# Cents is gotten to 2 decimal places by "":02d"
+print(f"The total amount of money is €{euros}.{cents:02d}")
